b6f7ee516d876afd5997827e6f9065a03936f1a4
rischan <rischanlab@gmail.com>
2014-04-28 15:49:14 -0700

    use account in array data

--- me/Main.php
+++ me/Main.php
@@ -3,10 +3,17 @@
 // set correct path!
 require_once 'getAPI.php';
 // change screen name to yours
-$tw_account ="kicaupemilu";
+$tw_account = array("alkwangju","kicaupemilu","rischanmafrur");
 //echo "Total Followers : " . getTwitterFollowers($tw_account) . ", Total Friends : " . getTwitterFollowings($tw_account) ;
-$data = getBio($tw_account);
-print_r($data);
+$tw_many = count($tw_account);
+
+for($i=0; $i<$tw_many; $i++){
+	print_r(getBio($tw_account[$i]));
+	
+}
+
+// $data = getBio($tw_account);
+// print_r($data);
 
 
 ?>
