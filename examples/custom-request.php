a4a651b8efbe0c2434c922c43162bf87a984dc18
rischan <rischanlab@gmail.com>
2014-04-28 11:00:39 -0700

    testing custom request

--- examples/custom-request.php
+++ examples/custom-request.php
@@ -7,7 +7,7 @@ include 'constants.php';
 $twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 
 // See https://dev.twitter.com/docs/api/1.1
-$statuses = $twitter->request('statuses/retweets_of_me', 'GET');
+$statuses = $twitter->request('statuses/retweets_of_me', 'GET', array('count' => 100));
 
 ?>
 <!doctype html>
@@ -15,11 +15,16 @@ $statuses = $twitter->request('statuses/retweets_of_me', 'GET');
 <title>Twitter retweets of me</title>
 
 <ul>
-<?php foreach ($statuses as $status): ?>
-	<li><a href="http://twitter.com/<?php echo $status->user->screen_name ?>"><img src="<?php echo htmlspecialchars($status->user->profile_image_url) ?>">
+	
+<?php 
+$i =1;
+foreach ($statuses as $status): ?>
+	<li><?php echo $i; ?><a href="http://twitter.com/<?php echo $status->user->screen_name ?>"><img src="<?php echo htmlspecialchars($status->user->profile_image_url) ?>">
 		<?php echo htmlspecialchars($status->user->name) ?></a>:
 		<?php echo Twitter::clickable($status) ?>
 		<small>at <?php echo date("j.n.Y H:i", strtotime($status->created_at)) ?></small>
 	</li>
-<?php endforeach ?>
+<?php 
+$i++;
+endforeach ?>
 </ul>
